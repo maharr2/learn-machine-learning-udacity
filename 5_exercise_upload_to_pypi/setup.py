edc98367e13fea08888a99121f71a3622cd11522
Rashmi Maharjan <Rashmi_Maharjan@gap.com>
2020-07-26 17:51:17 +0545

    initial commit

---
+++ 5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_binomial_distributions'],
+      author = 'Rashmi Maharjan',
+      author_email = 'rashmisinghmaharjan@gmail.com',
+      zip_safe=False)
